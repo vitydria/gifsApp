bc2f5e737e11d7331931abd13d947803f11e15f8
Victor Colmenares <colmenaresvictor2797@gmail.com>
2022-03-28 01:31:12 -0400

    section 7: vid 81

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SidebarComponent],
+  imports: [BrowserModule, SharedModule, GifsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
