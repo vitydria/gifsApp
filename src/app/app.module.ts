2ea4c4eb097804e21c03266e2b6bc836ab600172
Victor Colmenares <colmenaresvictor2797@gmail.com>
2022-03-28 15:40:06 -0400

    section 7: cap 81/fixed repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, SharedModule, GifsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/gifs/results/results.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styles: [],
+})
+export class ResultsComponent {
+  constructor() {}
+}
